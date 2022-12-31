edbe8d5f744416e616aaf4fa631c200db2554aef
feruzismoilov <ismoilovferuz0858@gmail.com>
2022-12-31 14:42:45 +0500

    imtihon masalalar

---
+++ js/main.js
@@ -0,0 +1,54 @@
+                            // 1-masala
+                               
+  var sum=0;
+  var n=parseInt(prompt("nechta son kiritmoqchisiz")) ;
+  for ( var i=0; i<n; i++){
+    var son = parseInt(prompt());
+    sum=sum+son;
+  }  
+  alert(sum) ;                         
+                               
+                               
+                               
+                               // 2-masala
+
+// var arr = ["jazz", "blues"];
+// console.log(arr);
+
+// arr.push("Rock-n-roll");   
+// console.log(arr);
+
+// if(arr.length%2==1){
+//     n = arr.length/2;
+//     console.log(arr.slice(n-0.5, n+0.5));
+// }else{
+//     console.log("nan");
+// }
+
+// arr.shift();
+// console.log(arr);
+
+// arr.unshift( "Rap","Reggae");
+// console.log(arr);
+
+
+                                        // 3-masala
+
+
+//   const salaries = {
+//     "Akmal": 1200,
+//     "Salim": 5200,
+//     "Karima": 1800
+//   };
+//   const values = Object.values(salaries);
+//   const sumSalaries = values.reduce((accumulator, value) => {
+//     return accumulator + value;
+//   }, 0);
+//   let sum = 0;
+
+//   for (const value of Object.values(salaries)) {
+//      sum += value;
+//   }
+  
+//   console.log(sumSalaries);
+  
